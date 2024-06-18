2a0177f65ef6c83179474a241fedea0f7c7f8a65
Dave <dave.gonzalez1992@outlook.com>
2024-06-18 12:44:45 -0500

    Primer commit. Planteamiento del reto.

---
+++ fibonacci.py
@@ -0,0 +1,4 @@
+"""Escriba un programa que imprima los primeros 50 números de la sucesión de Fibonacci, comenzando desde el 0.
+
+-La sucesión de Fibonacci se compone de una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores."""
+
