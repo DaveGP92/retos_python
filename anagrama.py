4d0f1bfd6554b31eb034d0e5508396795a5197bc
Dave <dave.gonzalez1992@outlook.com>
2024-06-17 22:50:51 -0500

    Dos formas de resolver el ejercicio

--- anagrama.py
+++ anagrama.py
@@ -7,3 +7,32 @@ Escribe un función que reciba dos argumentos "strings" y retorne True o False s
 
 -Dos palabras exactamente iguales no son anagramas."""
 
+palabra_1 = input("Escriba la primer palabra:\n").lower()
+
+palabra_2= input("Escriba la segunda palabra:\n").lower()
+
+
+#Forma 1
+
+def es_anagrama(palabra_1, palabra_2):
+
+    if palabra_1 == palabra_2:
+        return False
+    
+    elif sorted(palabra_1) == sorted(palabra_2):
+        return True
+
+    else:
+        return False
+
+respuesta = es_anagrama(palabra_1, palabra_2)
+
+print(f"¿Las palabras {palabra_1} y {palabra_2} son anagramas? = {respuesta}")
+
+
+
+#Forma 2 List Comprehension
+
+palabra_anagram = [False if palabra_1 == palabra_2 else True if sorted(palabra_1) == sorted(palabra_2) else False]
+
+print(f"¿Las palabras {palabra_1} y {palabra_2} son anagramas? = {palabra_anagram}")
