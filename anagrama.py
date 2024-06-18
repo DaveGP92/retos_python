0113a3063ef7faa80fdc6ebfa377ce35b286a58e
Dave <dave.gonzalez1992@outlook.com>
2024-06-17 21:36:44 -0500

    Primer commit. Creando el problema a resolver

---
+++ anagrama.py
@@ -0,0 +1,9 @@
+"""¿Es un anagrama?
+Escribe un función que reciba dos argumentos "strings" y retorne True o False según sean o no anagramas.
+
+-Un anagrama consiste en formar una palabra reordenando todas las letras de la palabra inicial.
+
+-No hace falta comprobar que ambas palabras existan.
+
+-Dos palabras exactamente iguales no son anagramas."""
+
