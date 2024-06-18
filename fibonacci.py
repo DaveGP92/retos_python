21d52503b8f5b4659500241ca0c2cdc8ab509e5b
Dave <dave.gonzalez1992@outlook.com>
2024-06-18 13:57:07 -0500

    Terminado

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,16 @@
 
 -La sucesión de Fibonacci se compone de una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores."""
 
+def fibonacci():
+
+    num_anterior = 0
+    num_siguiente = 1
+
+    for n in range(0,50):
+        resultado = num_anterior + num_siguiente
+        num_anterior = num_siguiente
+        num_siguiente = resultado
+        print(resultado)
+        
+
+fibonacci()
